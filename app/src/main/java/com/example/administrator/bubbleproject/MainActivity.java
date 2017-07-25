9d3b40310ffa0901a40f84492a5f6957b16077ad
Dyllen Nellis <dnellis0001@mymail.lausd.net>
2017-07-25 13:38:19 -0700

    Menu
    The menu is created and so are the Settings and Setup pages.

--- app/src/main/java/com/example/administrator/bubbleproject/MainActivity.java
+++ app/src/main/java/com/example/administrator/bubbleproject/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.administrator.bubbleproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -19,6 +21,32 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.id_setup) {
+            //Write own logic
+
+            Intent intentsetup=new Intent(MainActivity.this,setupActivity.class);
+            startActivity(intentsetup);
+            return true;
+        }
+        if (id==R.id.id_settings) {
+            //Write own logic
+
+            Intent intentsettings=new Intent(MainActivity.this,settingsActivity.class);
+            startActivity(intentsettings);
+            return true;
+        }
+        return true;
     }
 
 }
