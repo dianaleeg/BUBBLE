7edb8e03d6af4837ffc719150e961736dea6be74
Dyllen Nellis <dnellis0001@mymail.lausd.net>
2017-07-25 10:54:35 -0700

    first commit

---
+++ app/src/main/java/com/example/administrator/bubbleproject/tracker1PageActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.bubbleproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tracker1PageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracker1_page);
+
+        Intent intent = getIntent();
+
+    }
+}
